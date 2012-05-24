cc24e5131c4c0316f9923009b26cc60b33f77308
Lorenzo Sfarra <lorenzosfarra@gmail.com>
2012-05-24 12:15:28 +0200

    * Fixed errors in the headers of the bash scripts

--- bash/linode_quota.sh
+++ bash/linode_quota.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
-# This file is part of Zenity Player.
-# 
-# Zenity Player is free software: you can redistribute it and/or modify
-# it under the terms of the GNU  General Public License as published by
-# the Free Software Foundation, either version  2f the License, or
-# (at your option) any later version.
-# 
-# Zenity Player is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with Zenity Player.  If not, see <http://www.gnu.org/licenses/>.
-# 
-# 
 # This file is part of Linode Quota.
+# Author: Lorenzo Sfarra <lorenzosfarra@ubuntu.com>
 # 
 # Linode Quota is free software: you can redistribute it and/or modify
 # it under the terms of the GNU  General Public License as published by
