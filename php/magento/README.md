1289a7211fa0da1ce24f33f97c63fbfe25d85914
Lorenzo Sfarra <lorenzosfarra@gmail.com>
2016-01-30 12:54:47 +0100

    add order attribute

--- php/magento/README.md
+++ php/magento/README.md
@@ -4,5 +4,8 @@ It's a PHP script to retrieve *Magento* cron jobs and their status.
 # add_customer_attribute.php
 It's a PHP script to add a customer's new attribute.
 
+# add_order_attribute.php
+It's a PHP script to add an order's new attribute.
+
 # License
 Do whatever you want with it.
---
+++ php/magento/add_order_attribute.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Add an additional attribute in a Magento order.
+ * The example is about the platform from which the order was placed.
+ *
+ * Author: Lorenzo Sfarra <lorenzosfarra@gmail.com>
+ */
+
+require_once('app/Mage.php');
+Mage::app()->setCurrentStore(Mage::getModel('core/store')->load(Mage_Core_Model_App::ADMIN_STORE_ID));
+$installer = new Mage_Sales_Model_Mysql4_Setup;
+
+// The default platform is desktop
+$attribute_id = "order_platform";
+$attribute_label = "Order platform";
+$default_value = "desktop";
+
+$attribute  = array(
+        'type'          => 'varchar',
+        'backend_type'  => 'text',
+        'frontend_input' => 'text',
+        'is_user_defined' => true,
+        'label'         => $attribute_label, 
+        'visible'       => true,
+        'required'      => false,
+        'user_defined'  => false,   
+        'searchable'    => false,
+        'filterable'    => false,
+        'comparable'    => false,
+        'default'       => $default_value
+);
+
+$installer->addAttribute('order', $attribute_id, $attribute);
+$installer->endSetup();
+
+echo "Attribute $attribute_id added successfully.";
+
+?>
